82a29ac13187b6397f196ae9bb251e1511cc121b
tdubs <contact-tdubs@protonmail.com>
2021-08-31 13:11:55 -0700

    fixing delete endpoint

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -76,13 +76,13 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   try {
     const data = await Post.findById(req.params.id)
-    if (!data) {
-      res.status(404).json({message: 'The post with the specified ID does not exist'})
-    } else {
+    if (data) {
       const deleteIt = await Post.remove(req.params.id)
       if (deleteIt) {
         res.status(200).json(data)
       }
+    } else {
+      res.status(404).json({message: 'The post with the specified ID does not exist'})
     }
   } catch (e) {
     res.status(500).json({message: 'The post could not be removed'})
